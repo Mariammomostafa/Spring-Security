f1b7ff33340c34a2ce35d9cbb50a9bdbcec4750f
Your Name <you@example.com>
2025-12-09 17:50:09 +0200

    add

--- springMvc_Security_DBSave(Dao Layer)/src/main/java/com/selenium/springMvc/security/SecurityConfig.java
+++ springMvc_Security_DBSave(Dao Layer)/src/main/java/com/selenium/springMvc/security/SecurityConfig.java
@@ -7,10 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,42 +17,8 @@ public class SecurityConfig {
 	
 	@Autowired
 	private DataSource dataSource;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	// create users & save them permanently  in data base for first run
 
-//	@Bean
-//    public UserDetailsService userDetailsService() {
-//		
-//		
-//		 UserDetails user1 =User.builder()
-//	        		.username("mrmr")
-//	        		.password(passwordEncoder.encode("mrmr123"))
-//	        		.roles("ADMIN")
-//	        		.build();
-//		 
-//        UserDetails user =User.builder()
-//        		.username("hema")
-//        		.password(passwordEncoder.encode("hema123"))
-//        		.roles("USER")
-//        		.build();
-//        		        
-//        UserDetails admin = User.withUsername("monna")
-//            .password(passwordEncoder.encode("monna123"))
-//            .roles("USER")
-//            .build();
-//
-//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-//        jdbcUserDetailsManager.createUser(user);
-//        jdbcUserDetailsManager.createUser(admin);
-//        jdbcUserDetailsManager.createUser(user1);     
-//    
-//        return jdbcUserDetailsManager;
-//    }
-	
-	  // after saved users in DB & run app for the second time , need this been to load the saved users details from DB
+	  //need this been to load the saved users details from DB
 	  @Bean
 	  public UserDetailsService userDetailsService() {
 	
